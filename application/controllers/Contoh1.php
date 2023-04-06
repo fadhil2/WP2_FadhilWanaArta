a1001f0357b07116f6247ad4946bcd36d9812072
fadhilwanaarta <fadhilt23@gmail.com>
2023-04-06 16:38:26 +0700

    menambahkan .editorconfig,.gitignore,application/,composer.json,index.php,license.txt,readme.rst,system/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Fadhil Wana Arta
+ Saya kuliah di BSI Ciputat jurusan Teknologi Informatika";
+ }
+}
