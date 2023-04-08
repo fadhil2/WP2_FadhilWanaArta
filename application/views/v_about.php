258e39892e367afcb7fc54df32f48657db91a964
fadhilwanaarta <fadhilt23@gmail.com>
2023-04-08 12:32:43 +0700

    menambahkan application/controllers/Latihan1.php,application/controllers/Matakuliah.php,application/controllers/Web.php,application/models/Model_latihan1.php,application/views/v_about.php,application/views/v_footer.php,application/views/v_header.php,application/views/v_index.php,application/views/view-data-matakuliah.php,application/views/view-form-matakuliah.php,application/views/view-latihan.php,assets/

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<li><a href='<?php echo base_url().'index.php/web' ?>'></a></li>
+</a></li>
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type='square'>
+ <li>Nama Depan : Fadhil </li>
+ <li>Nama Tengah : Wana</li>
+ <li>Nama Belakang : Arta</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type='square'>
+ <li> Jl. hambergzwen, RT69/RW/69, Swiss</li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type='square'>
+ <li>Swiss</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type='square'>
+ <li>Golf</li>
+ <li>Balap Onta</li>
+ </ul>
+</section>
---
+++ application/views/view-latihan.php
@@ -0,0 +1,11 @@
+
+<head>
+ <title>Latihan 1</title>
+ </head>
+<body>
+ Halo lurr.. Belajar web programming kuy..!!!<br>
+ Nilai 1 = <?= $nilai1; ?>
+ Nilai 2 = <?= $nilai2; ?>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
